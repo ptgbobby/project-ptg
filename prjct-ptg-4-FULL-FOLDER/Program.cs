7665218f3489b5a2e290bcb68564bfca4d0ad66f
ptgbobby <45051440+ptgbobby@users.noreply.github.com>
2021-01-08 17:19:23 +0200

    Add files via upload

---
+++ prjct-ptg-4-FULL-FOLDER/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ptg_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello. Chose an option");
+            Console.WriteLine("1 - Patient result loger ");
+            Console.WriteLine("2 - Health Calculator");
+            int ress = int.Parse(Console.ReadLine());
+
+            if (ress == 1)
+            {
+
+                doctor doc = new doctor();
+                doc.name = "Boyko";
+                doc.username = "admin";
+                doc.pass = "admin";
+
+                Console.Write("Enter username: ");
+                string user = Console.ReadLine();
+                Console.Write("Enter password: ");
+                string pass = Console.ReadLine();
+                //login
+                if (user == doc.username && pass == doc.pass)
+                {
+                    doc.printhi();
+                    Console.Write("Enter patinet name: ");
+                    doc.pname = Console.ReadLine();
+                    Console.Write("Enter patinet age: ");
+                    doc.page = int.Parse(Console.ReadLine());
+                    Console.Write("Enter patinet test: ");
+                    doc.ptest = Console.ReadLine();
+                    Console.Write("Enter patinet res (Positive, +, positive, negative, -, Negative): ");
+                    doc.presults = Console.ReadLine();
+                    Console.WriteLine("");
+                    doc.resp();
+                }
+                else if (user != doc.username && pass != doc.pass)
+                {
+                    Console.WriteLine("Im sorry. Invalid data. You cant use this. Please type 1 to exit");
+                    int ans = int.Parse(Console.ReadLine());
+                    if (ans == 1)
+                    {
+                        Console.WriteLine("Press any key to exit.");
+                        Console.ReadKey();
+                    }
+                }
+                } else if (ress == 2) {
+                    calculator calc = new calculator();
+                    Console.Write("Enter your name: ");
+                    calc.name = Console.ReadLine();
+                    Console.Write("Enter your age: ");
+                    calc.age = int.Parse(Console.ReadLine());
+                    Console.Write("Enter your weight: ");
+                    calc.weight = int.Parse(Console.ReadLine());
+                    Console.Write("Enter your height: ");
+                    calc.height = int.Parse(Console.ReadLine());
+                    calc.print();
+                    calc.calculate();
+
+
+
+                } else if (ress != 1 || ress != 2)
+                {
+                    Console.WriteLine("Invalid input. Try with 1 or 2.");
+                } else
+                {
+                    Console.WriteLine("Unkown error!");
+                }
+            }
+        }
+    }
+
---
+++ prjct-ptg-4-FULL-FOLDER/calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace ptg_project
+{
+    public class calculator
+    {
+        public calculator()
+        {
+        }
+        public string name;
+        public int age;
+        public int weight;
+        public int height;
+
+        public void print()
+        {
+            Console.WriteLine($"Hello {name}. Enter your info.");
+        }
+
+        public void calculate()
+        {
+            if (height >= 160 && weight <= 70 && age > 10 || age < 40)
+            {
+                Console.WriteLine("All is fine");
+            } else if (height <= 160 && weight >= 70 && age > 10 || age < 40)
+            {
+                Console.WriteLine("Thats bad.");
+            } else if (weight > 100)
+            {
+                Console.WriteLine("Thats too bad.");
+            } else if (weight > 85 && age < 20)
+            {
+                Console.WriteLine("GO TO THE GYM");
+            } else if (age <= 10)
+            {
+                Console.WriteLine("You are small.");
+            } else if (weight < 70 && weight >= 45)
+            {
+                Console.WriteLine("Very good");
+            } else if ( weight < 45 && height < 130 && age < 10)
+            {
+                Console.WriteLine("You can not use this. Try with another value.");
+            } else if (age > 10 && age < 90 && weight > 30 && weight < 90 && height > 140 && height < 210)
+            {
+                Console.WriteLine("normal");
+            }
+            else
+            {
+                Console.WriteLine("Unkown error");
+            }
+        }
+    }
+}
